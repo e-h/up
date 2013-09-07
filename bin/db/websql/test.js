3ec5c8fff7aa607945b0518fb90c61831e32f567
e-h <feeblefollower2321@hotmail.ca>
2013-09-07 17:48:02 -0400

    fixed .gitignore; more spacing

--- bin/db/websql/test.js
+++ bin/db/websql/test.js
@@ -2,18 +2,17 @@
 var test.webdb    =   {};
 
 var test.webdb.open = function() {
-	var kByte  = 1024
-	var dbSize = kByte * kByte; // 1MB
-	test.webdb.db = openDatabase("Test", "1.0", "the red
-		fox jumps over the lazy brown dog", dbSize)
+  var kByte  = 1024;
+  var dbSize = kByte * kByte; // 1MB
+  test.webdb.db = openDatabase("Test", "1.0", "the red fox jumps over the lazy brown dog", dbSize);
 }
 
 test.webdb.onError = function handle_error (tx, e) {
-	// body...
-	alert("There has been an error: " + e.message)
+  // body...
+  alert("There has been an error: " + e.message);
 }
 
 test.webdb.onSuccess = function notify_user (tx, r) {
-	// body...
-	alert("Test db passed.")
+  // body...
+  alert("Test db passed.");
 }
